dd6c01b58d119ba01ebcb351f51405f6109d891a
CJL <2662442385@qq.com>
2025-11-07 12:30:04 +0800

    perf: 移除日志，提高性能

--- packages/markdown-mobile/vite.config.ts
+++ packages/markdown-mobile/vite.config.ts
@@ -2,28 +2,32 @@ import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@blocknote/core': fileURLToPath(new URL('../../blocknote/core/src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@blocknote/core': fileURLToPath(new URL('../../blocknote/core/src', import.meta.url)),
 
-      'markdown-operate': fileURLToPath(new URL('../markdown-operate/src', import.meta.url)),
-      'custom-blocknote-speaker': fileURLToPath(new URL('../../custom-blocknote/speaker/src', import.meta.url)),
-      'custom-blocknote-gradient-styles': fileURLToPath(new URL('../../custom-blocknote/gradient-styles/src', import.meta.url)),
+        'markdown-operate': fileURLToPath(new URL('../markdown-operate/src', import.meta.url)),
+        'custom-blocknote-speaker': fileURLToPath(new URL('../../custom-blocknote/speaker/src', import.meta.url)),
+        'custom-blocknote-gradient-styles': fileURLToPath(new URL('../../custom-blocknote/gradient-styles/src', import.meta.url)),
+      },
     },
-  },
-  base: './',
-  build: {
-    outDir: './dist',
-  },
-  server: {
-    port: 5180,
-    host: '::',
-  },
+    esbuild: {
+      drop: mode === 'production' ? ['console', 'debugger'] : [],
+    },
+
+    base: './',
+    build: {
+      outDir: './dist',
+    },
+    server: {
+      port: 5180,
+      host: '::',
+    },
+  }
 })
-
-
