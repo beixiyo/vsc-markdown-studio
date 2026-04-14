8c41e597ce5efd4971080aac67ff9b1947984d35
ES <2662442385@qq.com>
2026-04-14 18:39:41 +0800

    feat: 添加对 tiptap-api 的支持，更新相关依赖和配置

--- packages/markdown-mobile-tiptap/src/main.tsx
+++ packages/markdown-mobile-tiptap/src/main.tsx
@@ -1,5 +1,13 @@
 import ReactDOM from 'react-dom/client'
+import { LANGUAGES, TiptapI18nProvider } from 'tiptap-api/react'
 import App from './App'
 import './styles/index.scss'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TiptapI18nProvider
+    defaultLanguage={ LANGUAGES.ZH_CN }
+    storage={ { enabled: true, key: 'markdown-mobile-tiptap-language' } }
+  >
+    <App />
+  </TiptapI18nProvider>,
+)
